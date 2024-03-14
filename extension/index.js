14b28d99e97ad3bc8b20480637fe6187c6b6466c
Kenny Chu <kennychu0510@gmail.com>
2024-03-14 21:42:46 +0800

    revised image rendering method

--- extension/index.js
+++ extension/index.js
@@ -42,60 +42,98 @@
     return `${currentDomain}/sell_dsp.php?h_vid=${id}&h_vw=y`;
   }
 
-  console.log('28 car image preview script running');
+  console.log('28 car extension running');
 
   window.addEventListener('message', function (event) {
     if (typeof event.data !== 'string') return;
-    //console.log('FROM iFrame', event.data);
+    console.log('FROM iFrame', event.data);
+    try {
+      const data = JSON.parse(event.data);
+      if (data.type === 'car-image') {
+        const imageList = data.images;
+        const image = document.createElement('img');
+        image.id = 'car-image-' + data.index;
+        // let imageIndex = 0;
+        // image.addEventListener('click', (event) => {
+        //   event.stopPropagation();
+        //   imageIndex++;
+        //   if (imageIndex >= imageList.length) imageIndex = 0;
+        //   document.querySelector('#car-image-' + data.index).src = imageList[imageIndex];
+        // });
+        image.src = imageList[0];
+        image.style.width = '100%';
+        image.style.height = 'auto';
+        document.querySelector('#car-image-iframe-' + data.index).replaceWith(image);
+        document.querySelector('#car-image-placeholder-' + data.index).remove();
+      }
+    } catch (error) {}
   });
 
   const carList = document.querySelectorAll('[id^="rw_"]');
   for (let i = 0; i < carList.length; i++) {
     const queryHelper = CarListingQueryHelper(i);
-    const name = document.querySelector(queryHelper.name).textContent;
     const onClickFunction = document.querySelector(queryHelper.detailLink).getAttribute('onclick');
     const carId = getIdFromOnClickFunction(onClickFunction);
     const carDetailUrl = getCarDetailUrl(carId);
     const lastCol = document.querySelector(queryHelper.lastCol);
-    // console.log([i, name, carDetailUrl]);
+
     const div = document.createElement('div');
     div.style.width = '200px';
     div.style.height = '150px';
+    div.style.marginBottom = '5px';
+    div.style.border = '1px solid grey';
+    const divText = document.createElement('div');
+    divText.id = 'car-image-placeholder-' + i;
+    divText.textContent = 'Loading...';
+    div.style.display = 'flex';
+    div.style.justifyContent = 'center';
+    div.style.alignItems = 'center';
+    div.appendChild(divText);
+
+    const onDoneLoading = () => {
+      divText.remove();
+      iframe.style.zIndex = 'unset';
+      iframe.style.position = 'unset';
+    };
+
+    const updateDivText = (text) => {
+      divText.textContent = text;
+    };
+
     const iframe = document.createElement('iframe');
+    iframe.id = 'car-image-iframe-' + i;
     iframe.src = carDetailUrl;
     iframe.width = '100%';
     iframe.height = '100%';
     iframe.style.overflow = 'hidden';
+    iframe.style.zIndex = '-1';
+    iframe.style.position = 'absolute';
 
     div.appendChild(iframe);
     iframe.onload = function (event) {
-      window.parent.postMessage('Hello from iFrame ' + i, '*');
-      const anchor = iframe.contentDocument.querySelector(`a[href^="javascript:openPic(\'sell\', ${carId},"]`);
-      if (anchor) {
-        const firstImageSrc = anchor.querySelector('img').getAttribute('src');
-        const image = iframe.contentDocument.createElement('img');
-        image.src = firstImageSrc;
-        image.style.width = '100%';
-        image.style.height = 'auto';
-        iframe.contentDocument.body.replaceWith(image);
+      window.top.postMessage('Hello from iFrame ' + i, '*');
+      const anchors = iframe.contentDocument.querySelectorAll(`a[href^="javascript:openPic(\'sell\', ${carId},"]`);
+      if (anchors) {
+        const imageSrcList = [];
+        for (let anchor of anchors) {
+          imageSrcList.push(anchor.querySelector('img').getAttribute('src'));
+        }
+        window.top.postMessage(
+          JSON.stringify({
+            index: i,
+            images: imageSrcList,
+            type: 'car-image',
+          }),
+          '*'
+        );
       } else {
-        setTimeout(() => {
-          const anchor = iframe.contentDocument.querySelector(`a[href^="javascript:openPic(\'sell\', ${carId},"]`);
-          if (anchor) {
-            const firstImageSrc = anchor.querySelector('img').getAttribute('src');
-            const image = iframe.contentDocument.createElement('img');
-            image.src = firstImageSrc;
-            image.style.width = '100%';
-            image.style.height = 'auto';
-            iframe.contentDocument.body.replaceWith(image);
-          } else {
-            const span = iframe.contentDocument.createElement('span');
-            span.textContent = 'Failed to load image';
-            iframe.contentDocument.body.replaceWith(span);
-          }
-        }, 1000);
+        updateDivText('No image found');
       }
     };
     lastCol.replaceWith(div);
   }
 })();
+
+
+//src="https://djlfajk23a.28car.com/data/image/sell/2384000/2384724/369d9a48/2384724_3_s.jpg"
+//	https://djlfajk23a.28car.com/data/image/sell/2384000/2384724/369d9a48/2384724_m.jpg
